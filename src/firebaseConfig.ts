a215abda8db501667b92d00a1d517173e05dc5d3
ritikpandey655 <128251059+ritikpandey655@users.noreply.github.com>
2025-11-29 01:47:45 +0530

    feat: Update Firebase SDK and types
    Refactors Firebase initialization to use compat versions of SDK modules. Adds explicit type annotations for Firebase objects in service functions and `App.tsx` to improve type safety and developer experience.

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,7 @@
 
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJ48kwjfVfIm6Pi7v8Kc4fgd_PzZilZwY",
@@ -19,3 +19,5 @@ const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : fir
 export const auth = firebase.auth();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const db = firebase.firestore();
+
+export default firebase;
